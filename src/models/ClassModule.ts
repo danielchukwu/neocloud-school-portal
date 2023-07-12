87ebd8008ada37337e2bbcd40320492601894c37
dc <00danzy@gmail.com>
2023-07-12 23:17:26 +0100

    first commit

---
+++ src/models/ClassModule.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+// (){} : ! # _ => ""
+const ClassModuleSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true, max: 100 },
+  ClassId: { type: mongoose.Types.ObjectId, ref: 'Class', required: true, trim: true, max: 50 },
+}, { timestamps: true });
+
+const ClassModuleModel = mongoose.model('ClassModule', ClassModuleSchema);
+
+export default ClassModuleModel;
