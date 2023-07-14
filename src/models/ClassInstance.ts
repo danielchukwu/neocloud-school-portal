542a999c162c7fd54066bff2997fe540c2c7a988
dc <00danzy@gmail.com>
2023-07-13 23:21:04 -0200

    remove title

--- src/models/ClassInstance.ts
+++ src/models/ClassInstance.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 // (){} : ! # _ => ""
 const ClassInstanceSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true, max: 100 },
   isCompleted: { type: Boolean, required: true, default: false},
   classId: { type: mongoose.Types.ObjectId, ref: 'Class', required: true, max: 50 },
 }, { timestamps: true });
