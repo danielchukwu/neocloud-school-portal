2b64414d4dd1fe68e10c96a9169b0b6dcd920d53
dc <00danzy@gmail.com>
2023-07-13 23:21:42 -0200

    update field time to date name and add order field

--- src/models/ClassSchedule.ts
+++ src/models/ClassSchedule.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 // (){} : ! # _ => ""
 const ClassScheduleSchema = new mongoose.Schema({
+  order: { type: Number, required: true},
   title: { type: String, required: true, trim: true, max: 100 },
   description: { type: String, required: true, trim: true, max: 500, default: 'This is the description',},
-  time: { type: Date, required: true },
+  date: { type: Date, required: true },
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
   classModuleId: { type: mongoose.Types.ObjectId, ref: 'ClassModule', required: true, max: 50 },
