1effd2b70b29f347d18df8caacf3a14938778337
dc <00danzy@gmail.com>
2023-07-13 23:21:13 -0200

    add order field

--- src/models/ClassModule.ts
+++ src/models/ClassModule.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // (){} : ! # _ => ""
 const ClassModuleSchema = new mongoose.Schema({
+  order: { type: Number, required: true},
   title: { type: String, required: true, trim: true, max: 100 },
   ClassId: { type: mongoose.Types.ObjectId, ref: 'Class', required: true, trim: true, max: 50 },
 }, { timestamps: true });
