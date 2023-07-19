be920900ba9639779446378482206e4c98bf9e7b
dc <00danzy@gmail.com>
2023-07-19 21:55:49 +0100

    fix: field name from facultiesId to facultyId

--- src/models/UsersFacultiesRoles.ts
+++ src/models/UsersFacultiesRoles.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // (){} : ! # _ => ""
 const UsersFacultiesRolesSchema = new mongoose.Schema({
   userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true, trim: true, max: 50 },
-  facultiesId: { type: mongoose.Types.ObjectId, ref: 'Faculty', required: true, trim: true, max: 50 },
+  facultyId: { type: mongoose.Types.ObjectId, ref: 'Faculty', required: true, trim: true, max: 50 },
   roleId: { type: mongoose.Types.ObjectId, ref: 'Role', required: true, trim: true, max: 50 },  // Users roleId
 });
 
