f6ca2866dd88b796101d10841c61b9363a65f4dd
dc <00danzy@gmail.com>
2023-07-20 09:30:20 +0100

    improve: improve code readability

--- src/models/Class.ts
+++ src/models/Class.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const ClassSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true, max: 100 },
   avatar: { type: String, trim: true, max: 200 },
-  about: { type: String, required: true, trim: true, max: 500, default: 'This is the description',},
+  about: { type: String, required: true, trim: true, max: 500, default: 'This is the description', },
   facultyId: { type: mongoose.Types.ObjectId, ref: 'Faculty', required: true, trim: true, max: 50 },
 }, { timestamps: true });
 
