3927968264a4ee1ef7f79b8f4066924af523804f
dc <00danzy@gmail.com>
2023-07-20 14:22:06 +0100

    update: add educatorId field

--- dist/models/ClassInstance.js
+++ dist/models/ClassInstance.js
@@ -7,6 +7,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 // (){} : ! # _ => ""
 const ClassInstanceSchema = new mongoose_1.default.Schema({
     isCompleted: { type: Boolean, required: true, default: false },
+    educatorId: { type: mongoose_1.default.Types.ObjectId, ref: 'User', required: true, max: 50 },
     classId: { type: mongoose_1.default.Types.ObjectId, ref: 'Class', required: true, max: 50 },
 }, { timestamps: true });
 const ClassInstanceModel = mongoose_1.default.model('ClassInstance', ClassInstanceSchema);
