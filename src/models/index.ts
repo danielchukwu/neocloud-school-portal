0eeeea8636eb7cf3d225dd987c4b2582557cb4fd
dc <00danzy@gmail.com>
2023-08-12 17:18:32 +0100

    first commit: implement barrel patter import and export

---
+++ src/models/index.ts
@@ -0,0 +1,31 @@
+import Attendance from "../models/Attendance";
+import Class from "../models/Class";
+import ClassInstance from "../models/ClassInstance";
+import ClassModule from "../models/ClassModule";
+import ClassSchedule from "../models/ClassSchedule";
+import Classwork from "../models/Classwork";
+import Faculty from "../models/Faculty";
+import Notification from "../models/Notification";
+import NotificationType from "../models/NotificationType";
+import Role from "../models/Role";
+import User from "../models/User";
+import UsersClassesRoles from "../models/UsersClassesRoles";
+import UsersFacultiesRoles from "../models/UsersFacultiesRoles";
+import UsersRoles from "../models/UsersRoles";
+
+export {
+  Attendance,
+  Class,
+  ClassInstance,
+  ClassModule,
+  ClassSchedule,
+  Classwork,
+  Faculty,
+  Notification,
+  NotificationType,
+  Role,
+  User,
+  UsersClassesRoles,
+  UsersFacultiesRoles,
+  UsersRoles,
+};
