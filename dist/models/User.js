7f9083356b395c9c1c104ff01c528195e466fe58
dc <00danzy@gmail.com>
2023-08-12 17:19:42 +0100

    improve: use UserType and create Access and Refresh tokens in login method

--- dist/models/User.js
+++ dist/models/User.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const validator_1 = __importDefault(require("validator"));
-// (){} : ! # _ => ""
+const jwt_1 = require("../jwt");
 const UserSchema = new mongoose_1.default.Schema({
     name: { type: String, required: [true, 'Name field is required'], trim: true, maxLength: [30, 'Name must be less than 30 characters'] },
     email: { type: String, required: [true, 'Email field is required'], trim: true, maxLength: [50, 'Email must be less than 50 characters'], unique: true, validate: [validator_1.default.isEmail, 'Enter a valid email'] },
@@ -23,7 +23,7 @@ const UserSchema = new mongoose_1.default.Schema({
     cover: { type: String, trim: true, maxLength: [200, 'Cover string must be less than 50 characters'] },
     bio: { type: String, required: true, trim: true, maxLength: [500, 'Bio must be less than 500 characters'], default: 'Ready to connect' },
     phone: { type: String, required: false, trim: true, maxLength: [25, 'Phone should be less than 25 characters'], minLength: [10, 'Phone number should be more than 9 characters'] },
-    password: { type: String, required: [true, 'Password field is required'], trim: true, validate: [validator_1.default.isStrongPassword, 'Password must include at least 1 uppercase, 1 lowercase, and 1 special character and should be a minimum of 6 characters'] },
+    password: { type: String, required: [true, 'Password field is required'], trim: true, validate: [validator_1.default.isStrongPassword, "Password must contain one from each A-Z, a-z, (@_\$!%*?&)"] },
     roleId: { type: mongoose_1.default.Types.ObjectId, ref: 'Role', maxLength: 50 },
 }, { timestamps: true });
 UserSchema.pre('save', function (next) {
@@ -38,10 +38,15 @@ UserSchema.statics.login = function (email, password) {
         const user = yield this.findOne({ email });
         if (user) {
             const passwordMatches = yield bcrypt_1.default.compare(password, user.password);
-            if (passwordMatches)
-                return user;
+            if (passwordMatches) {
+                const { access_token, refresh_token } = yield (0, jwt_1.createAccessAndRefreshToken)(user);
+                return { access_token, refresh_token, user };
+            }
+            ;
+            // If you change the text below, also change the text in handleError to catch this error whenever it is thrown
             throw new Error('Incorrect password!');
         }
+        // If you change the text below, also change the text in handleError to catch this error whenever it is thrown
         throw new Error("The email doesn't exist!");
     });
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,20 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import { createAccessAndRefreshToken, createJWT } from "../jwt";
+import { UserType } from "../types/model_types";
 
-// (){} : ! # _ => ""
-const UserSchema = new mongoose.Schema({
+
+const UserSchema = new mongoose.Schema<UserType>({
   name: { type: String, required: [true, 'Name field is required'], trim: true, maxLength: [30, 'Name must be less than 30 characters'] },
   email: { type: String, required: [true, 'Email field is required'], trim: true, maxLength: [50, 'Email must be less than 50 characters'], unique: true, validate: [validator.isEmail, 'Enter a valid email'] },
   avatar: { type: String, trim: true, maxLength: [200, 'Avatar string must be less than 200 characters'] },
   cover: { type: String, trim: true, maxLength: [200, 'Cover string must be less than 50 characters'] },
   bio: { type: String, required: true, trim: true, maxLength: [500, 'Bio must be less than 500 characters'], default: 'Ready to connect'},
   phone: { type: String, required: false, trim: true, maxLength: [25, 'Phone should be less than 25 characters'], minLength: [10, 'Phone number should be more than 9 characters'] },
-  password: { type: String, required: [true, 'Password field is required'], trim: true, validate: [validator.isStrongPassword, 'Password must include at least 1 uppercase, 1 lowercase, and 1 special character and should be a minimum of 6 characters'] },
+  password: { type: String, required: [true, 'Password field is required'], trim: true, validate: [validator.isStrongPassword, "Password must contain one from each A-Z, a-z, (@_\$!%*?&)"] },
   roleId: { type: mongoose.Types.ObjectId, ref: 'Role', maxLength: 50 },
 }, { timestamps: true });
-
-interface UserDocument extends mongoose.Document {}
-
-interface UserModel extends mongoose.Model<UserDocument> {
-  login(email: string, password: string): Promise<this>;
-}
 
 UserSchema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt();
@@ -26,18 +22,26 @@ UserSchema.pre('save', async function(next) {
   next();
 });
 
+interface UserModel extends mongoose.Model<UserType> {
+  login(email: string, password: string): Promise<this>;
+}
+
 UserSchema.statics.login = async function(email: string, password: string) {
   const user = await this.findOne({ email });
   if (user) {
     const passwordMatches = await bcrypt.compare(password, user.password);
-    if (passwordMatches) return user;
+    if (passwordMatches) {
+      const {access_token, refresh_token} = await createAccessAndRefreshToken(user);
+      return {access_token, refresh_token, user};
+    };
+    // If you change the text below, also change the text in handleError to catch this error whenever it is thrown
     throw new Error('Incorrect password!');
   }
+  // If you change the text below, also change the text in handleError to catch this error whenever it is thrown
   throw new Error("The email doesn't exist!");
 };
 
 
-
-const userModel = mongoose.model<UserDocument, UserModel>('User', UserSchema);
+const userModel = mongoose.model<UserType, UserModel>('User', UserSchema);
 
 export default userModel;
