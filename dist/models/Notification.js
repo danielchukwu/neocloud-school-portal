9f959ad3340d16770c00f2da0c9ea63ed0765339
dc <00danzy@gmail.com>
2023-08-16 09:01:14 +0100

    update: add new field 'ownerId'

--- dist/models/Notification.js
+++ dist/models/Notification.js
@@ -9,6 +9,7 @@ const NotificationSchema = new mongoose_1.default.Schema({
     body: { type: String, required: true, trim: true, maxLength: 200 },
     notificationTypeId: { type: mongoose_1.default.Types.ObjectId, ref: 'User', required: true, maxLength: 50 },
     userId: { type: mongoose_1.default.Types.ObjectId, ref: 'User', maxLength: 50 },
+    ownerId: { type: mongoose_1.default.Types.ObjectId, ref: 'User', maxLength: 50 },
     classworkId: { type: mongoose_1.default.Types.ObjectId, ref: 'Classwork', maxLength: 50 },
     classInstanceId: { type: mongoose_1.default.Types.ObjectId, ref: 'ClassInstance', maxLength: 50 },
     facultyId: { type: mongoose_1.default.Types.ObjectId, ref: 'Faculty', maxLength: 50 }, // can be null
