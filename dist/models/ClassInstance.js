851c9b4d93a5fd98bb4b2ecdff8841b2adf4400f
dc <00danzy@gmail.com>
2023-09-23 10:00:32 +0100

    update: add unique to name field

--- dist/models/ClassInstance.js
+++ dist/models/ClassInstance.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 // (){} : ! # _ => ""
 const ClassInstanceSchema = new mongoose_1.default.Schema({
-    isCompleted: { type: Boolean, required: true, default: false },
+    isCompleted: { type: Boolean, required: true, unique: true, default: false },
     educatorId: { type: mongoose_1.default.Types.ObjectId, ref: 'User', required: true, maxLength: 50 },
     classId: { type: mongoose_1.default.Types.ObjectId, ref: 'Class', required: true, maxLength: 50 },
 }, { timestamps: true });
